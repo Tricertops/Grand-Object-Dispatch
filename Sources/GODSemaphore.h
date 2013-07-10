909461a537a2cc1ea66a70f2a16b9e09d5bdb3ab
Martin Kiss <martin.kiss@me.com>
2013-07-10 09:40:04 +0200

    Implement Semaphore

--- Sources/GODSemaphore.h
+++ Sources/GODSemaphore.h
@@ -12,17 +12,44 @@
 
 
 
+/// Wrapper for dispatch_semaphore_t.
+/// A dispatch semaphore is an efficient implementation of a traditional counting semaphore. Dispatch semaphores call down to the kernel only when the calling thread needs to be blocked. If the calling semaphore does not need to block, no kernel call is made.
 @interface GODSemaphore : GODObject
 
 
-- (instancetype)initWithValue:(NSUInteger)value;
 
-- (NSUInteger)signal;
-- (NSUInteger)wait:(NSTimeInterval)timeout;
-- (NSUInteger)waitDate:(NSTimeInterval)timeout;
+#pragma mark Creating Semaphores
 
+/// Creates new counting semaphore with an initial value.
+- (instancetype)initWithValue:(NSInteger)value;
+
+
+
+#pragma mark Using Semaphores
+
+/// Signals (increments) a semaphore.
+- (NSInteger)signal;
+
+/// Waits for (decrements) a semaphore for given time.
+- (NSInteger)wait:(NSTimeInterval)timeout;
+
+/// Waits for (decrements) a semaphore forever.
+- (void)wait;
+
+/// Waits for (decrements) a semaphore until given date.
+- (NSInteger)waitDate:(NSDate *)date;
+
+
+
+#pragma mark Protected Methods
+/// You should try to avoid using these methods, since this abtraction is trying to cover them. They may become private in future.
+
+/// Designated initializer.
 - (instancetype)initWithDispatchSemaphore:(dispatch_semaphore_t)dispatchSemaphore;
+
+/// Underlaying dispatch semaphore.
 - (dispatch_semaphore_t)dispatchSemaphore;
+
 
 
 @end
--- Sources/GODSemaphore.m
+++ Sources/GODSemaphore.m
@@ -8,6 +8,99 @@
 
 #import "GODSemaphore.h"
 
+
+
+
+
+@interface GODSemaphore ()
+
+
+@property (nonatomic, readwrite, strong) dispatch_semaphore_t dispatchSemaphore;
+
+
+@end
+
+
+
+
+
+
+
+
+
+
 @implementation GODSemaphore
+
+
+
+
+
+#pragma mark Superclass Overrides
+
+
+- (dispatch_object_t)dispatchObject {
+    return self.dispatchSemaphore;
+}
+
+
+
+
+
+#pragma mark Creating Semaphores
+
+
+- (instancetype)initWithValue:(NSInteger)value {
+    return [self initWithDispatchSemaphore:dispatch_semaphore_create(value)];
+}
+
+
+
+
+
+#pragma mark Using Semaphores
+
+
+- (NSInteger)signal {
+    return dispatch_semaphore_signal(self.dispatchSemaphore);
+}
+
+
+- (NSInteger)wait:(NSTimeInterval)timeout {
+    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(timeout * NSEC_PER_SEC));
+    return dispatch_semaphore_wait(self.dispatchSemaphore, time);
+}
+
+
+- (void)wait {
+    dispatch_semaphore_wait(self.dispatchSemaphore, DISPATCH_TIME_FOREVER);
+    // No return value, because it can only succeed. Or lock forever.
+}
+
+
+- (NSInteger)waitDate:(NSDate *)date {
+    return [self wait:date.timeIntervalSinceNow];
+}
+
+
+
+
+
+#pragma mark Protected Methods
+
+
+- (instancetype)initWithDispatchSemaphore:(dispatch_semaphore_t)dispatchSemaphore {
+    self = [super init];
+    if (self) {
+        NSParameterAssert(dispatchSemaphore != nil);
+        if ( ! dispatchSemaphore) return nil;
+        
+        self.dispatchSemaphore = dispatchSemaphore;
+    }
+    return self;
+}
+
+
+
+
 
 @end
