eb00fb77d4b92a0f5923775576f1c60fadad9107
Martin Kiss <martin.kiss@me.com>
2013-07-10 19:59:30 +0200

    Add Timer-specific event block

--- Sources/GODTimer.h
+++ Sources/GODTimer.h
@@ -8,7 +8,7 @@
 
 #import "GODSource.h"
 
-
+typedef void(^GODTimerHandler)(NSUInteger fireCount);
 
 
 /// Convenience wrapper for creating dispatch sources of type `timer'.
@@ -19,12 +19,18 @@
 #pragma mark Creating Timer
 
 /// Creates a new configured timer Source.
-- (instancetype)initWithDate:(NSDate *)date interval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODBlock)block;
+- (instancetype)initWithDate:(NSDate *)date interval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODTimerHandler)block;
 
 /// Returns started timer Source with given arguments.
-+ (instancetype)startedTimerWithInterval:(NSTimeInterval)interval queue:(GODQueue *)queue handler:(GODBlock)block;
-+ (instancetype)startedTimerWithInterval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODBlock)block;
-+ (instancetype)startedTimerWithDate:(NSDate *)date interval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODBlock)block;
++ (instancetype)startedTimerWithInterval:(NSTimeInterval)interval queue:(GODQueue *)queue handler:(GODTimerHandler)block;
++ (instancetype)startedTimerWithInterval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODTimerHandler)block;
++ (instancetype)startedTimerWithDate:(NSDate *)date interval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODTimerHandler)block;
+
+
+#pragma mark Handling Timer
+
+/// Sets the event handler block for the given dispatch source. Sends the number of times the timer has fired since the last handler invocation.
+- (void)setEventTimerHandler:(GODTimerHandler)block;
 
 
 
--- Sources/GODTimer.m
+++ Sources/GODTimer.m
@@ -23,34 +23,41 @@
 #pragma mark Creating Timer
 
 
-- (instancetype)initWithDate:(NSDate *)date interval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODBlock)block {
+- (instancetype)initWithDate:(NSDate *)date interval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODTimerHandler)block {
     self = [super initWithType:GODSourceTypeTimer handle:0 mask:0 queue:queue];
     if (self) {
         [self setTimerStart:date interval:interval leeway:leeway];
-        [self setEventHandler:block];
+        [self setEventTimerHandler:block];
     }
     return self;
 }
 
 
-+ (instancetype)startedTimerWithInterval:(NSTimeInterval)interval queue:(GODQueue *)queue handler:(GODBlock)block {
++ (instancetype)startedTimerWithInterval:(NSTimeInterval)interval queue:(GODQueue *)queue handler:(GODTimerHandler)block {
     return [self startedTimerWithDate:nil interval:interval leeway:0 queue:queue handler:block];
 }
 
 
-+ (instancetype)startedTimerWithInterval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODBlock)block {
++ (instancetype)startedTimerWithInterval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODTimerHandler)block {
     return [self startedTimerWithDate:nil interval:interval leeway:leeway queue:queue handler:block];
 }
 
 
-+ (instancetype)startedTimerWithDate:(NSDate *)date interval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODBlock)block {
++ (instancetype)startedTimerWithDate:(NSDate *)date interval:(NSTimeInterval)interval leeway:(NSTimeInterval)leeway queue:(GODQueue *)queue handler:(GODTimerHandler)block {
     GODTimer *timer = [[self alloc] initWithDate:date interval:interval leeway:leeway queue:queue handler:block];
     [timer start];
     return timer;
 }
 
 
-
+- (void)setEventTimerHandler:(GODTimerHandler)block {
+    __weak typeof(self) weakSelf = self;
+    [self setEventHandler:^{
+        // number of times the timer has fired since the last handler invocation
+        NSUInteger fireCount = weakSelf.data;
+        block(fireCount);
+    }];
+}
 
 
 @end
