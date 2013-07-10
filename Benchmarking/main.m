e0ee30ff97cb52171aa497b88a1abf0121ac4859
Martin Kiss <martin.kiss@me.com>
2013-07-10 09:42:47 +0200

    Add Benchmarking target

---
+++ Benchmarking/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Benchmarking
+//
+//  Created by Martin Kiss on 10.7.13.
+//  Copyright (c) 2013 Martin Kiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
+
