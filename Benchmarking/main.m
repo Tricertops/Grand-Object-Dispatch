cb9f219d299ab31b4bf88fb32476862c1825ea38
Martin Kiss <martin.kiss@me.com>
2013-07-10 14:41:54 +0200

    Add Benchmarking code, that tests three ways of loop iteration.
    This is not related to the GCD wrapper itself, it is just for fun.

--- Benchmarking/main.m
+++ Benchmarking/main.m
@@ -7,15 +7,62 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "GrandObjectDispatch.h"
 
 
-
+void godb_linear(NSUInteger count);
+void godb_parallel(NSUInteger count);
+void godb_serial(NSUInteger count);
+void godb_iteration(NSUInteger index);
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        NSUInteger benchmarkCount = 1000;
+        NSUInteger iterationCount = 10000;
+        
+        NSLog(@"Linear: %f s", [GODObject benchmark:benchmarkCount block:^{
+            godb_linear(iterationCount);
+        }]);
+        
+        NSLog(@"Parallel: %f s", [GODObject benchmark:benchmarkCount block:^{
+            godb_parallel(iterationCount);
+        }]);
+        
+        NSLog(@"Serial: %f s", [GODObject benchmark:benchmarkCount block:^{
+            godb_serial(iterationCount);
+        }]);
+        
     }
     return 0;
 }
 
+
+void godb_linear(NSUInteger count) {
+    for (NSUInteger index = 0; index < count; index++) {
+        godb_iteration(index);
+    }
+}
+
+void godb_parallel(NSUInteger count) {
+    GODQueue *queue = [[GODQueue alloc] initWithName:@"com.MartinKiss.GrandObjectDispatch.benchmarkQueue.parallel" concurrent:YES];
+    [queue apply:count block:^(NSUInteger index) {
+        godb_iteration(index);
+    }];
+    [queue barrierSync:^{}];
+}
+
+void godb_serial(NSUInteger count) {
+    GODQueue *queue = [[GODQueue alloc] initWithName:@"com.MartinKiss.GrandObjectDispatch.benchmarkQueue.serial" concurrent:NO];
+    [queue apply:count block:^(NSUInteger index) {
+        godb_iteration(index);
+    }];
+    [queue barrierSync:^{}];
+}
+
+void godb_iteration(NSUInteger index) {
+    // I needed some expensinve operation...
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    // ... but didn't want to use regexes.
+}
